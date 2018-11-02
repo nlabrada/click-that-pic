fe1c02cd2f123207a8eab33f0afa393bb1aa28e6
nlabrada <nlabrada12@gmail.com>
2018-11-02 18:28:03 -0500

    Updates

---
+++ precache-manifest.315f7bd2c5c70b69c18fefa07b7292f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b05ad9a22ff394e067de878679e4f7a",
+    "url": "/click-that-pic/static/media/toStart.1b05ad9a.png"
+  },
+  {
+    "revision": "96c74f41717dd1bfaa76f945963606e3",
+    "url": "/click-that-pic/static/media/sunny.96c74f41.png"
+  },
+  {
+    "revision": "08b309617cfd30891314",
+    "url": "/click-that-pic/static/js/runtime~main.08b30961.js"
+  },
+  {
+    "revision": "6565873d1d5c1f5c2ee5",
+    "url": "/click-that-pic/static/js/main.6565873d.chunk.js"
+  },
+  {
+    "revision": "6d0fddbf21aa63bd2fae",
+    "url": "/click-that-pic/static/js/1.6d0fddbf.chunk.js"
+  },
+  {
+    "revision": "6565873d1d5c1f5c2ee5",
+    "url": "/click-that-pic/static/css/main.038e058c.chunk.css"
+  },
+  {
+    "revision": "5a7de24189539698ec24c40c909a579a",
+    "url": "/click-that-pic/index.html"
+  }
+];
